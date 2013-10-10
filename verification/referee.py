8893b6fb4d4bdd7b9884c0f85f77090268ff644c
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-10-10 22:42:34 +1200

    referee and tests

--- verification/referee.py
+++ verification/referee.py
@@ -1,49 +1,84 @@
-"""
-CheckiOReferee is a base referee for checking you code.
-    arguments:
-        tests -- the dict contains tests in the specific structure.
-            You can find an example in tests.py.
-        cover_code -- is a wrapper for the user function and additional operations before give data
-            in the user function. You can use some predefined codes from checkio.referee.cover_codes
-        checker -- is replacement for the default checking of an user function result. If given, then
-            instead simple "==" will be using the checker function which return tuple with result
-            (false or true) and some additional info (some message).
-            You can use some predefined codes from checkio.referee.checkers
-        add_allowed_modules -- additional module which will be allowed for your task.
-        add_close_builtins -- some closed builtin words, as example, if you want, you can close "eval"
-        remove_allowed_modules -- close standard library modules, as example "math"
-
-checkio.referee.checkers
-    checkers.float_comparison -- Checking function fabric for check result with float numbers.
-        Syntax: checkers.float_comparison(digits) -- where "digits" is a quantity of significant
-            digits after coma.
-
-checkio.referee.cover_codes
-    cover_codes.unwrap_args -- Your "input" from test can be given as a list. if you want unwrap this
-        before user function calling, then using this function. For example: if your test's input
-        is [2, 2] and you use this cover_code, then user function will be called as checkio(2, 2)
-    cover_codes.unwrap_kwargs -- the same as unwrap_kwargs, but unwrap dict.
-
-"""
-
+from math import hypot
+import random
 from checkio.signals import ON_CONNECT
 from checkio import api
-from checkio.referees.io import CheckiOReferee
-from checkio.referees import cover_codes
-from checkio.referees import checkers
+from checkio.referees.multicall import CheckiORefereeMulti
 
 from tests import TESTS
 
+MAX_STEP = 30
+
+DIRS = {
+    "N": -4,
+    "S": 4,
+    "E": 1,
+    "W": -1
+}
+
+
+def initial_referee(data):
+    data["stephan"] = 16
+    data["ghost"] = 1
+    data['step_count'] = 0
+    return data
+
+
+def process_referee(referee_data, user_result):
+    referee_data['step_count'] += 1
+    stephen = referee_data["stephen"]
+    ghost = referee_data["ghost"]
+    house = referee_data["house"]
+    if referee_data['step_count'] > MAX_STEP:
+        referee_data.update({"result": False, "result_addon": "Too many moves."})
+        return referee_data
+    if not isinstance(user_result, str) or user_result not in "NSWE":
+        referee_data.update({"result": False, "result_addon": 'The function should return "N", "S", "W" or "E".'})
+        return referee_data
+    if user_result in referee_data["house"][stephen - 1]:
+        referee_data.update({"result": False, "result_addon": 'Stefan ran into a closed door. It was hurt.'})
+        return referee_data
+    if stephen == 1 and user_result == "N":
+        referee_data.update({"result": True, "result_addon": 'Stefan has escaped.', 'stephen': 0})
+        return referee_data
+    stephen += DIRS[user_result]
+    if ((user_result == "W" and stephen % 4 == 1) or (user_result == "E" and stephen % 4 == 0) or
+            (stephen < 1) or (stephen > 16)):
+        referee_data.update({"result": False, "result_addon": 'Stefan has gone out into the unknown.'})
+        return referee_data
+    referee_data["stephen"] = stephen
+    sx, sy = stephen % 4, ((stephen - 1) // 4) + 1
+    ghost_dirs = [ch for ch in "NWES" if ch not in house[ghost - 1]]
+    if ghost % 4 == 1 and "W" in ghost_dirs:
+        ghost_dirs.remove("W")
+    if ghost % 4 == 0 and "E" in ghost_dirs:
+        ghost_dirs.remove("E")
+    ghost_dist = ["", 1000]
+    for d in ghost_dirs:
+        new_ghost = ghost + DIRS[d]
+        gx, gy = new_ghost % 4, ((new_ghost - 1) // 4) + 1
+        dist = (gx - sx) ** 2 + (gy - sy) ** 2
+        if ghost_dist[1] > dist:
+            ghost_dist = [d, dist]
+        elif ghost_dist[1] == dist:
+            ghost_dist[0] += d
+    ghost_move = random.choice(ghost_dist[0])
+    ghost += DIRS[ghost_move]
+    referee_data.update({"result": False,
+                         "result_addon": 'Stefan has gone out into the unknown.',
+                         "ghost": ghost,
+                         "ghost_move": ghost_move})
+    return referee_data
+
+
+def is_win_referee(referee_data):
+    return referee_data['stephen'] == 0
+
+
 api.add_listener(
     ON_CONNECT,
-    CheckiOReferee(
+    CheckiORefereeMulti(
         tests=TESTS,
-        cover_code={
-            'python-27': cover_codes.unwrap_args,  # or None
-            'python-3': cover_codes.unwrap_args
-        },
-        # checker=None,  # checkers.float.comparison(2)
-        # add_allowed_modules=[],
-        # add_close_builtins=[],
-        # remove_allowed_modules=[]
+        initial_referee=initial_referee,
+        process_referee=process_referee,
+        is_win_referee=is_win_referee,
     ).on_ready)
--- verification/tests.py
+++ verification/tests.py
@@ -7,30 +7,19 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
-    "Basics": [
-        {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
-        },
-        {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
-    ],
-    "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
-    ]
+    "Basics": {
+        "house": [
+            "", "S", "S", "",
+            "E", "NW", "NS", "",
+            "E", "WS", "NS", "",
+            "", "N", "N", ""]
+    },
+    "Extra": {
+        "house": [
+            "", "", "", "",
+            "E", "ESW", "ESW", "W",
+            "E", "ENW", "ENW", "W",
+            "", "N", "N", ""]
+    }
 }
